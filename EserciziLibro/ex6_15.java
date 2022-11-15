06a7118e9dddd28057ca2b1f11cc0bb37521c473
davidebuggin <94127166+davidebuggin@users.noreply.github.com>
2022-11-15 12:59:56 +0100

    done 6.15

---
+++ EserciziLibro/ex6_15.java
@@ -0,0 +1,26 @@
+
+import EsempiLibro.Quadrato;
+import prog.io.*;
+
+public class ex6_15 {
+
+    public static void main(String[] args) {
+
+        ConsoleInputManager in = new ConsoleInputManager();
+        ConsoleOutputManager out = new ConsoleOutputManager();
+        int[] lati = new int[5];
+        for (int i = 0; i < lati.length; i++) {
+            lati[i] = in.readInt("inserire lato: ");
+        }
+        Quadrato[] quadrati = new Quadrato[lati.length];
+        for (int i = 0; i < quadrati.length; i++) {
+            Quadrato quadrato1 = new Quadrato(lati[i]);
+            quadrati[i] = quadrato1;
+        }
+        for (int i = 0; i < quadrati.length; i++) {
+            out.println(quadrati[i].toString());
+        }
+
+    }
+
+}
